91b802e09dc77660edbc7c610d1756c7db56577b
Josh Loftis <jshlfts32@gmail.com>
2017-09-03 18:45:41 -0400

    Setting up base html and js

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,16 @@
+//Global variables
+//=============================================================================
+//var(s) for questions
+//var(s) for answers
+//var for correct count
+//var for incorrect count
+//var for current question count
+
+
+//Functions variables
+//=============================================================================
+
+
+
+//Game Logic
+//=============================================================================
