3807df0f0c2a8b6033506905e9636e26f706273e
Josh Loftis <jshlfts32@gmail.com>
2017-09-05 23:18:23 -0400

    Refactoring js

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -10,36 +10,104 @@ var questionData = {
 var correctCount = 0;
 //var for incorrect count
 var incorrectCount = 0;
+//var for no answer
+var blank = 0;
 //var for current question count
 var questionIndex = 0;
 var timePerQuestion = 5;
+var remainingTime;
+var intervalId;
+var nextQuestion;
 
 
 //Functions
 //=============================================================================
 
 //function to start game
+function startGame() {
   //on click of start game button, game loads
-  //clear start button
-  //add question and list of answers
+  $('.startGame').on('click', function() {
+    //clear start button
+    $('#btnDiv').empty();
+    //add question and list of answers
+    return gameDisplay();
+  });
+}
 
 // function to display time, questions, and answers on screen
+function gameDisplay() {
   //display timer
-  //display questions
+  remainingTime = setTimeout(evalAnswer, (timePerQuestion * 1000));
+  questionTimer(); count(); evalAnswer();
+  clearTimeout(nextQuestion);
+  $('#answerScreen').empty();
+  //display questions=
+  $('#question').html('<div><p class="questionText">' + questionData.question[questionIndex] + '</p></div>');
   //display answers
+  for (var i=0; i<questionData.answerList[questionIndex].length; i++) {
+    $('#answers').append('<li class="anAnswer">' + questionData.answerList[questionIndex][i] + '</li>');
+  }
+}
 
 //function to evaluate user answer (correct, incorrect, no answer)
-  //if the page times out, store answer as undefined
+function evalAnswer() {
+  //if the question times out, store answer as undefined
+  if (timePerQuestion < 1) {
     //show screen telling the user the correct answer
-  //if the user clicks the correct answer, store answer as correct answer
-    //show screen congratulating the user
-  //if the user clicks the incorrect answer, store answer as incorrect answer
-    //show screen telling the user their answer was wrong and what the correct answer was
+    clearTimeout(remainingTime);
+    clearInterval(intervalId);
+    blank++;
+    $('#timeRemaining, #question, #answers').empty();
+    $('#answerScreen').html('<div><p class="message">Ooh, time\'s up!</p></div><div><p>The correct answer was ' + questionData.correctAns[questionIndex] + '.</p></div>');
+    questionIndex++;
+    nextQuestion = setTimeout(gameDisplay, 4000);
+  }
+  $(document).on('click', '.anAnswer', function() {
+      //if the user clicks the correct answer, store answer as correct answer
+    if ($(this).html() == questionData.correctAns[questionIndex]) {
+      //show screen congratulating the user
+      clearTimeout(remainingTime);
+      clearInterval(intervalId);
+      correctCount++;
+      $('#timeRemaining, #question, #answers').empty();
+      $('#answerScreen').html('<div><p class="message">Yes! That was correct.</p></div><div><p>' + questionData.correctAns[questionIndex] + ' was the correct answer! Great job!</p></div>');
+      questionIndex++;
+      nextQuestion = setTimeout(gameDisplay, 4000);
+    } //if the user clicks the incorrect answer, store answer as incorrect answer
+    else {
+      //show screen telling the user their answer was wrong and what the correct answer was
+      clearTimeout(remainingTime);
+      clearInterval(intervalId);
+      $('#timeRemaining, #question, #answers').empty();
+      $('#answerScreen').html('<div><p class="message">Nope! Wrong answer!</p></div><div><p>The correct answer was ' + questionData.correctAns[questionIndex] + '.</p></div>');
+      questionIndex++;
+      nextQuestion = setTimeout(gameDisplay, 4000);
+    }
+  });
+}
 
 //evaluate game
+function endGame() {
   //when no questions are left, empty the screen
+  if (questionIndex == questionData.question.length) {
+  //clear out all screens
+  $('#answerScreen').empty();
   //show number of correct answers
   //show number of incorrect answers
   //show number of non-answered questions
+  $('#gameOver').html('<div><p class="message">Game Over!</p></div><div><p>You answered ' + correctCount + ' questions correctly.</p></div><div><p>You answered ' + incorrectCount + ' questions incorrect.</p></div><div><p>You did not answer ' + blank + ' questions.</p></div><div><button class="restartGame" onclick="restartGame()>Restart Game</button></div>"');
   //show restart game button
-  
+  }
+}
+
+function count() {
+  intervalId = setInterval(questionTimer, 1000);
+}
+function questionTimer() {
+  timePerQuestion--;
+  $('#timeRemaining').html(timePerQuestion);
+}
+
+$(document).ready(function() {
+  startGame();
+});
