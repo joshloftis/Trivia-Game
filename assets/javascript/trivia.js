144e17c3265584033c226d936336a692ff983596
Josh Loftis <jshlfts32@gmail.com>
2017-09-05 21:44:40 -0400

    Wiping slate clean, refactoring, and pseudo coding

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -3,7 +3,7 @@
 //var(s) for questions and answers
 var questionData = {
   question: ["What is Harry's last name?", "In to which house was Harry placed?", "Who tried to kill Harry in the first book?", "Who is Harry's godfather?", "How many Harry Potter books are there?", "Who is Harry's ancestor?", "Who was Ron's first girlfriend?", "Who killed Sirius Black?"],
-  answer: [["Potter", "Dursley", "Weasley", "Granger"],["Slytherin", "Ravenclaw", "Gryffindor", "Hufflepuff"], ["Professor Dumbledore", "Ron Weasley", "Reubeus Hagrid", "Lord Voldemort"], ["James Potter", "Sirius Black", "Peter Pettigrew", "Alastor Moody"], [1, 4, 5, 7], ["Salazar Slytherin", "Godric Gryffindor", "Helga Hufflepuff", "Rowena Ravenclaw"], ["Hermione Graner", "Lavendar Brown", "Parvati Patel", "Milicent Boulstrod"], ["Severus Snape", "Lily Potter", "Narcissa Malfoy", "Belatrix Lestrange"]],
+  answerList: [["Potter", "Dursley", "Weasley", "Granger"],["Slytherin", "Ravenclaw", "Gryffindor", "Hufflepuff"], ["Professor Dumbledore", "Ron Weasley", "Reubeus Hagrid", "Lord Voldemort"], ["James Potter", "Sirius Black", "Peter Pettigrew", "Alastor Moody"], [1, 4, 5, 7], ["Salazar Slytherin", "Godric Gryffindor", "Helga Hufflepuff", "Rowena Ravenclaw"], ["Hermione Graner", "Lavendar Brown", "Parvati Patel", "Milicent Boulstrod"], ["Severus Snape", "Lily Potter", "Narcissa Malfoy", "Belatrix Lestrange"]],
   correctAns: ["Potter", "Gryffindor", "Lord Voldemort", "Sirius Black", 7, "Godric Gryffindor", "Lavendar Brown", "Belatrix Lestrange"]
 };
 //var for correct count
@@ -11,112 +11,35 @@ var correctCount = 0;
 //var for incorrect count
 var incorrectCount = 0;
 //var for current question count
-var currentQuestion = 0;
+var questionIndex = 0;
 var timePerQuestion = 5;
-var remainingTime;
-var wrongAnswer;
-var intervalId;
-var theQuestionTime;
-var resultScreen;
+
 
 //Functions
 //=============================================================================
-//changing on screen content functions=========================================
-function startGame() {
-  $('.startGame').on('click', function() {
-    initGame();
-  });
-}
 
-function restartGame() {
-  $('.restartGame').on('click', function() {
-    $('#timeRemaining, #question, #answers, #answerScreen, #gameOver').empty();
-    correctCount = 0;
-    incorrectCount = 0;
-    currentQuestion = 0;
-    initGame();
-  });
-}
+//function to start game
+  //on click of start game button, game loads
+  //clear start button
+  //add question and list of answers
 
-function initGame() {
-  count(); displayTime(); questions();
-  $('#btnDiv').empty();
-  remainingTime = $('#timeRemaining').html(displayTime);
-}
+// function to display time, questions, and answers on screen
+  //display timer
+  //display questions
+  //display answers
 
-function nextQuestion() {
-  $('#answerScreen').empty();
-  clearTimeOut(resultScreen);
-  timePerQuestion = 5;
-  currentQuestion++;
-  if (currentQuestion == questionData.question.length) {
-    evalGame();
-  } else {
-    displayTime(); count(); timeOutQuestion(); questions();
-  }
-}
+//function to evaluate user answer (correct, incorrect, no answer)
+  //if the page times out, store answer as undefined
+    //show screen telling the user the correct answer
+  //if the user clicks the correct answer, store answer as correct answer
+    //show screen congratulating the user
+  //if the user clicks the incorrect answer, store answer as incorrect answer
+    //show screen telling the user their answer was wrong and what the correct answer was
 
-function questions() {
-  $('#question').html('<h2 class="questionText">' + questionData.question[currentQuestion] + '<h2>');
-  for (var i=0; i < questionData.answer[currentQuestion].length; i++) {
-    $('#answers').append('<li class="anAnswer">' + questionData.answer[currentQuestion][i] + '</li>');
-  }
-}
-
- function timeRanOut() {
-   clearTimeout(theQuestionTime);
-   clearInterval(intervalId);
-   resultsTimer();
-   $('#timeRemaining, #question, #answers').empty();
-   $('#answerScreen').html("<div><p class='message'>Oh. Time's up!<p>The correct answer was " + questionData.correctAns[currentQuestion] + '.</p></div>');
- }
-
-
-
-//evaluation functions=========================================================
-function evalAnswer() {
-  clearTimeout(theQuestionTime);
-  clearInterval(intervalId);
-  resultsTimer();
-  if ($(this).html() == questionData.correctAns[currentQuestion]) {
-    correctCount++;
-    $('#timeRemaining, #question, #answers').empty();
-    $('#answerScreen').html("<div><p class='message'>Yes! That's correct!</p><p>" + questionData.correctAns[currentQuestion] + ' was the correct answer! Great job!</p></div>');
-  } else {
-    incorrectCount++;
-    wrongAnswer = $(this).html();
-    $('#timeRemaining, #question, #answers').empty();
-    $('#answerScreen').html('<div><p class="message">Nope '+ wrongAnswer + ' is not the correct answer!</p><p>The correct answer was ' + questionData.correctAns[currentQuestion] + '.</p></div>');
-  }
-}
-
-function evalGame() {
-  clearInterval(intervalId);
-  clearTimeout(theQuestionTime);
-  $('#timeRemaining, #question, #answers').empty();
-  $('#gameOver').html('<div><p class="message">Game Over</p><p class="endScreenText">You answered ' + correctCount + ' questions correctly!</p></div><div><p class="endScreenText">You answered ' + incorrectCount + ' questions incorrectly!</p></div><div><button class="restartGame" onclick="restartGame()">Restart Game</div>"');
-}
-
-//Timing functions=============================================================
-function count(){
-  intervalId = setInterval(displayTime, 1000);
-}
-
-function timeOutQuestion() {
-  theQuestionTime = setTimeout(evalAnswer, (timePerQuestion * 1000));
-}
-
-function resultsTimer() {
-  resultScreen = setTimeout(nextQuestion, 4000);
-}
-
-function displayTime() {
-  timePerQuestion--;
-  $('#timeRemaining').html('<h3>' + timePerQuestion + '</h3>');
-}
-
-//Game Start
-//=============================================================================
-$(document).ready(function() {
-  startGame();
-});
+//evaluate game
+  //when no questions are left, empty the screen
+  //show number of correct answers
+  //show number of incorrect answers
+  //show number of non-answered questions
+  //show restart game button
+  
